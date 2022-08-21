7daa3d2692a096f6fc8360ca60e42c789104cd85
MissDaizy <diana.ukrainsky@s.afeka.ac.il>
2022-08-21 12:27:48 +0300

    First commit of Todo List App.

---
+++ app/src/main/java/com/diana_ukrainsky/todolist/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.diana_ukrainsky.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    // promise to Kotlin that we'll initialize this later
+    private lateinit var todoAdapter: TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Empty list because we want the list to be empty at first
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle) // False by default
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
---
+++ app/src/main/java/com/diana_ukrainsky/todolist/Todo.kt
@@ -0,0 +1,13 @@
+package com.diana_ukrainsky.todolist
+// Class just to hold data
+data class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+
+
+
+
+
+
+
+)
